9b65c063714eabfab2fa92baf585b7a7dfb17cdd
Sasha Khinevich <hinevics@gmail.com>
2021-11-01 01:27:24 +0300

    update gitignore

--- db/datascript.py
+++ db/datascript.py
@@ -1,31 +1,26 @@
 import os
 import requests
-from config import MONGO_HOST
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
-# from db.tools.datageneration import generation
-# from db.dev_settings import MONGO_NUMBER, MONGO_HOST, MONGO_PORT, MONGO_DATABASE, MONGO_COLLECTION
+import config
 
-print(MONGO_HOST)
-
-# API_FISH_TEXT = r'https://fish-text.ru/get?format=json&number=5'
-# API_NAME = r'https://api.namefake.com/'
+def name_generator():
+    return requests.get(url=config.API_NAME).json()['name']
 
 
-# def name_generator():
-#     return requests.get(url=API_NAME).json()['name']
+def text_generator():
+    return requests.get(url=config.API_FISH_TEXT).json()['text']
 
 
-# def text_generator():
-#     return requests.get(url=API_FISH_TEXT).json()['text']
+def generation(number):
+    for i in range(number):
+        yield dict(author=name_generator(), text=text_generator())
 
 
-# def generation(number):
-#     for i in range(number):
-#         yield dict(author=name_generator(), text=text_generator())
+client = MongoClient(host=config.MONGO_HOST, port=config.MONGO_PORT)
+db = client[config.MONGO_DATABASE]
+series_collection = db[config.MONGO_COLLECTION]
 
-# client = MongoClient(host=MONGO_HOST, port=MONGO_PORT)
-# db = client[MONGO_DATABASE]
-# series_collection = db[MONGO_COLLECTION]
-# data = [i for i in generation(MONGO_NUMBER)]
-# series_collection.insert_many(data)
+data = [i for i in generation(config.MONGO_NUMBER)]
+
+series_collection.insert_many(data)
